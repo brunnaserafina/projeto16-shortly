915b750c69d76525e285ec64b8d28c768ce5b69c
Brunna Serafina <brunnaserafina@gmail.com>
2022-10-11 15:26:12 -0300

    build: intalling express, cors, dotenv and testing GET status

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+
+server.get("/status", (req, res) => res.send("Ok!"));
+
+server.listen(process.env.PORT, () =>
+  console.log(`Listening on port ${process.env.PORT}`)
+);
