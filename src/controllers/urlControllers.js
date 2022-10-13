817b5bbcb6f13a8abca4d59f30004f409fa88ab4
Brunna Serafina <brunnaserafina@gmail.com>
2022-10-13 15:33:35 -0300

    feat: returning data from url by id in route GET '/urls/:id'

--- src/controllers/urlControllers.js
+++ src/controllers/urlControllers.js
@@ -24,3 +24,23 @@ export async function shortenLink(req, res) {
   }
 }
 
+export async function searchShortLink(req, res) {
+  const { id } = req.params;
+
+  try {
+    const existingId = (
+      await connection.query(`SELECT * FROM links WHERE id=$1;`, [id])
+    ).rows[0];
+
+    if (!existingId) {
+      return res.sendStatus(STATUS_CODE.NOT_FOUND);
+    }
+
+    return res
+      .status(STATUS_CODE.OK)
+      .send({ id, shortUrl: existingId.short_url, url: existingId.link_url });
+  } catch (err) {
+    console.error(err);
+    return res.sendStatus(STATUS_CODE.SERVER_ERROR);
+  }
+}
--- src/routes/urlRouter.js
+++ src/routes/urlRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
 
-import { shortenLink } from "../controllers/urlControllers.js";
+import { shortenLink, searchShortLink } from "../controllers/urlControllers.js";
 import { urlValidate } from "../middlewares/schemaMiddlewares.js";
 import { tokenValidate } from "../middlewares/authMiddlewares.js";
 
 const router = express.Router();
 
 router.post("/urls/shorten", tokenValidate, urlValidate, shortenLink);
+router.get("/urls/:id", searchShortLink);
 
 export default router;
