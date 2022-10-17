d0ca5843431f3c0061d90793337815d273b8cce2
Brunna Serafina <brunnaserafina@gmail.com>
2022-10-17 13:31:02 -0300

    Prepare for deploy

--- src/databases/pgsql.js
+++ src/databases/pgsql.js
@@ -5,19 +5,11 @@ dotenv.config();
 
 const { Pool } = pg;
 
-// const connection = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-// });
-
 const connection = new Pool({
-  host: "localhost",
-  port: 5432,
-  user: "postgres",
-  password: process.env.PASSWORD,
-  database: "shortly",
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default connection;
